109ad9357d3f43e583ed11df6bcc0efa49cbafbc
deepakprasad119 <48093750+deepakprasad119@users.noreply.github.com>
2020-04-07 16:02:47 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -38,9 +38,9 @@ def sonarScan(){
 }
 	
 def dbScriptDeploy(){
-	stage('dbScriptDeploy') {
-			// One or more steps need to be included within the steps block.
-			flywayrunner commandLineArgs: '', credentialsId: 'mysql', flywayCommand: 'info', installationName: 'Flyway', locations: 'src\\main\\resources\\db\\migration', url: 'jdbc:mysql://127.0.0.1:3306/'
+    stage('dbScriptDeploy') {
+	// One or more steps need to be included within the steps block.
+	flywayrunner commandLineArgs: '', credentialsId: 'mysql', flywayCommand: 'info', installationName: 'Flyway', locations: 'src\\main\\resources\\db\\migration', url: 'jdbc:mysql://127.0.0.1:3306/'
     }
 }
 
