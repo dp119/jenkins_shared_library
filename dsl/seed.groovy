fa7c9f8a775c59d99fc704281e586afc7dc095c3
deepakprasad119 <48093750+deepakprasad119@users.noreply.github.com>
2020-04-08 09:45:17 +0530

    Update seed.groovy

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,3 +1,32 @@
-job(jobName) {
-    description("A simple Freestyle Job created from seed.groovy")
+def createDeploymentJob(jobName, repoUrl) {
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                        }
+                        branches('master')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
 }
+
+
+def buildPipelineJobs() {
+    def repo = "https://github.com/dp119/"
+    def repoUrl = repo + jobName + ".git"
+    def deployName = jobName + "_deploy"
+    def testName = jobName + "_test"
+
+    createDeploymentJob(deployName, repoUrl)
+}
+
+buildPipelineJobs()
